cf029f95bc7a51d8273ccc78a09f006976a54a21
Alex Mihalașcu <alexmihalascu@gmail.com>
2023-12-14 18:14:38 +0200

    shortfix

--- user_management.php
+++ user_management.php
@@ -86,7 +86,7 @@ $result = $conn->query($sql);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>User Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
